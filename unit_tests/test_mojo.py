dab2e2fa23e210df48d19a5fb8a60966a5a862cc
Chris MacNaughton <chris.macnaughton@canonical.com>
2018-04-30 17:48:28 +0200

    fix style

--- unit_tests/test_mojo.py
+++ unit_tests/test_mojo.py
@@ -9,8 +9,14 @@ class TestModel(ut_utils.BaseTestCase):
 
     def test_mojo_matrix_view(self):
         client = mock.MagicMock()
-        client.get_jobs.return_value = [{'_class': 'hudson.model.FreeStyleProject', 'name': 'test_job',
-                                         'url': 'http://127.0.0.1:8080/job/test_job/', 'color': 'blue', 'fullname': 'test_job'}]
+        client.get_jobs.return_value = [
+            {
+                '_class': 'hudson.model.FreeStyleProject',
+                'name': 'test_job',
+                'url': 'http://127.0.0.1:8080/job/test_job/',
+                'color': 'blue',
+                'fullname': 'test_job'
+            }]
         jobs = mojo.mojo_matrix(client)
         client.get_jobs.assert_called_with(view_name='MojoMatrix')
         self.assertEqual(len(jobs), 1)
