98d0d8e0036716a6eebc10d09cbbe1291f756f73
Chris MacNaughton <chris.macnaughton@canonical.com>
2018-05-01 08:59:14 +0200

    more tests

--- unit_tests/test_uosci_jenkins.py
+++ unit_tests/test_uosci_jenkins.py
@@ -8,7 +8,7 @@ class TestModel(unittest.TestCase):
         super(TestModel, self).setUp()
 
     @mock.patch.object(uosci_jenkins, 'Jenkins')
-    def test_matrix_view(self, _jenkins):
+    def test_init(self, _jenkins):
         uosci_jenkins.Jenkins(
             'http://127.0.0.1:8080',
             username='test_user',
@@ -17,3 +17,16 @@ class TestModel(unittest.TestCase):
             'http://127.0.0.1:8080',
             username='test_user',
             password='test_password')
+
+    # @mock.patch.object(uosci_jenkins, 'Jenkins')
+    @mock.patch("uosci.uosci_jenkins.jenkins.Jenkins.get_jobs")
+    def test_matrix_view(self, _get_jobs):
+        j_client = mock.MagicMock()
+        uosci_jenkins.jenkins.Jenkins = j_client
+        client = uosci_jenkins.Jenkins(
+            'http://127.0.0.1:8080',
+            username='test_user',
+            password='test_password')
+        # client.get_jobs.return_value = mock.MagicMock()
+        client.matrix('test')
+        _get_jobs.assert_called_with(view_name='test')
