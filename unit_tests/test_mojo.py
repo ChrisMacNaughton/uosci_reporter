619a7fb9b184907c1e6b31932983cfb5badf2f1f
Chris MacNaughton <chris.macnaughton@canonical.com>
2018-05-08 16:27:28 +0200

    break out name filtering method

--- unit_tests/test_mojo.py
+++ unit_tests/test_mojo.py
@@ -70,3 +70,12 @@ class TestModel(unittest.TestCase):
             password=None,
             filter='testing',
         )
+
+    def test_filter_job_name_with_test(self):
+        self.assertFalse(mojo.filter_job("test_something"))
+        self.assertTrue(mojo.filter_job("not-included"))
+
+    def test_filter_job_name_with_filter(self):
+        self.assertFalse(mojo.filter_job("test_something"))
+        self.assertTrue(mojo.filter_job("test_something", "else"))
+
